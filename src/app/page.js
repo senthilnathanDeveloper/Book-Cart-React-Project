767237727fbbf4b1dd024059d31f48141d2ef97d
gowthu1218 <grsriy2002@gmail.com>
2025-01-23 09:50:03 +0530

    product list api is integrated

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Header from "./components/Header";
+// src/app/page.js
+'use client';
+import Image from 'next/image';
+import styles from './page.module.css';
+import Header from './components/Header';
+import ProductList from './components/ProductList';
+import { Provider } from 'react-redux';
+import store from '@/redux/store';
 
 export default function Home() {
   return (
-  <div>
-    <Header/>
-  </div>
+<>
+    <Provider store={store}>
+      <Header />
+      <ProductList />
+ 
+    </Provider>
+</>
   );
 }
